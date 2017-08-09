835e242a127b8011236af9206139e15b48c6c36c
Bill Hsiao <bill.hsiao@billhsiao.com>
2017-08-09 13:10:31 -0700

    dotenv

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 web: node index.js
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+require('dotenv').config();
 var index = require('./routes/index');
 var users = require('./routes/users');
 
